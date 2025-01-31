78cb747164c78ec1fcce50d4f79d4acaff3f8d24
JaydenHuang8 <165731293+JaydenHuang8@users.noreply.github.com>
2025-01-30 17:27:58 -0800

    log projects

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { fetchJSON, renderProjects,  } from './global.js';
 //fetchGithubData
 const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
-console.log(latestProjects)
+console.log(latestProjects);
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(latestProjects, projectsContainer, 'h2');
